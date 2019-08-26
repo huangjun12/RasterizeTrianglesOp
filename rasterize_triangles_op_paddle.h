d5dd168f2d0beaf1469ab752b2d03e8bea46715b
huangjun12 <2399845970@qq.com>
2019-08-26 11:45:40 +0800

    modify forward var name

--- rasterize_triangles_op_paddle.h
+++ rasterize_triangles_op_paddle.h
@@ -5,16 +5,10 @@
 #include "paddle/fluid/framework/eigen.h"
 
 namespace {
-// Threshold for a barycentric coordinate triplet's sum, below which the
-// coordinates at a pixel are deemed degenerate. Most such degenerate triplets
-// in an image will be exactly zero, as this is how pixels outside the mesh
-// are rendered.
+// Threshold for a barycentric coordinate triplet's sum.
 constexpr float kDegenerateBarycentricCoordinatesCutoff = 0.9f;
 
-// If the area of a triangle is very small in screen space, the corner vertices
-// are approaching colinearity, and we should drop the gradient to avoid
-// numerical instability (in particular, blowup, as the forward pass computation
-// already only has 8 bits of precision).
+// The minimum area of a triangle in screen space.
 constexpr float kMinimumTriangleArea = 1e-13;
 
 }  //namespace
@@ -27,53 +21,38 @@ using Tensor = framework::Tensor;
 
 namespace {
 
-// Takes the minimum of a, b, and c, rounds down, and converts to an integer
-// in the range [low, high].
+// Takes the minimum, rounds down, and converts to an integer in the range [low, high].
 inline int ClampedIntegerMin(float a, float b, float c, int low, int high) {
     return std::min(
             std::max(static_cast<int>(std::floor(std::min(std::min(a, b), c))),
                      low), high);
 }
 
-// Takes the maximum of a, b, and c, rounds up, and converts to an integer
-// in the range [low, high].
+// Takes the maximum, rounds up, and converts to an integer in the range [low, high].
 inline int ClampedIntegerMax(float a, float b, float c, int low, int high) {
     return std::min(
             std::max(static_cast<int>(std::ceil(std::max(std::max(a, b), c))), low),
             high);
 }
 
-// Computes a 3x3 matrix inverse without dividing by the determinant.
-// Instead, makes an unnormalized matrix inverse with the correct sign
-// by flipping the sign of the matrix if the determinant is negative.
-// By leaving out determinant division, the rows of M^-1 only depend on two out
-// of three of the columns of M; i.e., the first row of M^-1 only depends on the
-// second and third columns of M, the second only depends on the first and
-// third, etc. This means we can compute edge functions for two neighboring
-// triangles independently and produce exactly the same numerical result up to
-// the sign. This in turn means we can avoid cracks in rasterization without
-// using fixed-point arithmetic.
-// See http://mathworld.wolfram.com/MatrixInverse.html
-void ComputeUnnormalizedMatrixInverse(const float a11, const float a12,
-                                      const float a13, const float a21,
-                                      const float a22, const float a23,
-                                      const float a31, const float a32,
-                                      const float a33, float m_inv[9]) {
-    m_inv[0] = a22 * a33 - a32 * a23;
-    m_inv[1] = a13 * a32 - a33 * a12;
-    m_inv[2] = a12 * a23 - a22 * a13;
-    m_inv[3] = a23 * a31 - a33 * a21;
-    m_inv[4] = a11 * a33 - a31 * a13;
-    m_inv[5] = a13 * a21 - a23 * a11;
-    m_inv[6] = a21 * a32 - a31 * a22;
-    m_inv[7] = a12 * a31 - a32 * a11;
-    m_inv[8] = a11 * a22 - a21 * a12;
+// Computes a 3x3 matrix inverse (http://mathworld.wolfram.com/MatrixInverse.html).
+void ComputeUnnormalizedMatrixInverse(const float m11, const float m12,
+                                      const float m13, const float m21,
+                                      const float m22, const float m23,
+                                      const float m31, const float m32,
+                                      const float m33, float m_inv[9]) {
+    m_inv[0] = m22 * m33 - m32 * m23;
+    m_inv[1] = m13 * m32 - m33 * m12;
+    m_inv[2] = m12 * m23 - m22 * m13;
+    m_inv[3] = m23 * m31 - m33 * m21;
+    m_inv[4] = m11 * m33 - m31 * m13;
+    m_inv[5] = m13 * m21 - m23 * m11;
+    m_inv[6] = m21 * m32 - m31 * m22;
+    m_inv[7] = m12 * m31 - m32 * m11;
+    m_inv[8] = m11 * m22 - m21 * m12;
 
-    // The first column of the unnormalized M^-1 contains intermediate values for
-    // det(M).
     const float det = a11 * m_inv[0] + a12 * m_inv[3] + a13 * m_inv[6];
 
-    // Transfer the sign of the determinant.
     if (det < 0.0f) {
         for (int i = 0; i < 9; ++i) {
             m_inv[i] = -m_inv[i];
@@ -81,31 +60,21 @@ void ComputeUnnormalizedMatrixInverse(const float a11, const float a12,
     }
 }
 
-
 // Computes the edge functions from M^-1 as described by Olano and Greer,
 // "Triangle Scan Conversion using 2D Homogeneous Coordinates."
-//
-// This function combines equations (3) and (4). It first computes
-// [a b c] = u_i * M^-1, where u_0 = [1 0 0], u_1 = [0 1 0], etc.,
-// then computes edge_i = aX + bY + c
-void ComputeEdgeFunctions(const float px, const float py, const float m_inv[9],
+void ComputeEdgeFunctions(const float pixel_x, const float pixel_y, const float m_inv[9],
                           float values[3]) {
     for (int i = 0; i < 3; ++i) {
         const float a = m_inv[3 * i + 0];
         const float b = m_inv[3 * i + 1];
         const float c = m_inv[3 * i + 2];
 
-        values[i] = a * px + b * py + c;
+        values[i] = a * pixel_x + b * pixel_y + c;
     }
 }
 
 // Determines whether the point p lies inside a front-facing triangle.
-// Counts pixels exactly on an edge as inside the triangle, as long as the
-// triangle is not degenerate. Degenerate (zero-area) triangles always fail the
-// inside test.
 bool PixelIsInsideTriangle(const float edge_values[3]) {
-    // Check that the edge values are all non-negative and that at least one is
-    // positive (triangle is non-degenerate).
     return (edge_values[0] >= 0 && edge_values[1] >= 0 && edge_values[2] >= 0) &&
            (edge_values[0] > 0 || edge_values[1] > 0 || edge_values[2] > 0);
 }
@@ -139,91 +108,85 @@ public:
         float* barycentric_coordinates = bc_eigen.data();
         float* z_buffer = zb_eigen.data();
 
-        //start compute
+        // Start computing
         const float half_image_width = 0.5 * image_width;
         const float half_image_height = 0.5 * image_height;
         float unnormalized_matrix_inverse[9];
-        float b_over_w[3];
+        float edge_w[3];
 
         for (int triangle_id = 0; triangle_id < triangle_count; ++triangle_id) {
-            const int v0_x_id = 4 * triangles[3 * triangle_id];
-            const int v1_x_id = 4 * triangles[3 * triangle_id + 1];
-            const int v2_x_id = 4 * triangles[3 * triangle_id + 2];
+            const int vx0_id = 4 * triangles[3 * triangle_id];
+            const int vx1_id = 4 * triangles[3 * triangle_id + 1];
+            const int vx2_id = 4 * triangles[3 * triangle_id + 2];
 
-            const float v0w = vertices[v0_x_id + 3];
-            const float v1w = vertices[v1_x_id + 3];
-            const float v2w = vertices[v2_x_id + 3];
-            // Early exit: if all w < 0, triangle is entirely behind the eye.
-            if (v0w < 0 && v1w < 0 && v2w < 0) {
+            const float vw0 = vertices[vx0_id + 3];
+            const float vw1 = vertices[vx1_id + 3];
+            const float vw2 = vertices[vx2_id + 3];
+            // if all w < 0, triangle is entirely behind the eye.
+            if (vw0 < 0 && vw1 < 0 && vw2 < 0) {
                 continue;
             }
 
-            const float v0x = vertices[v0_x_id];
-            const float v0y = vertices[v0_x_id + 1];
-            const float v1x = vertices[v1_x_id];
-            const float v1y = vertices[v1_x_id + 1];
-            const float v2x = vertices[v2_x_id];
-            const float v2y = vertices[v2_x_id + 1];
+            const float vx0 = vertices[vx0_id];
+            const float vy0 = vertices[vx0_id + 1];
+            const float vx1 = vertices[vx1_id];
+            const float vy1 = vertices[vx1_id + 1];
+            const float vx2 = vertices[vx2_id];
+            const float vy2 = vertices[vx2_id + 1];
 
-            ComputeUnnormalizedMatrixInverse(v0x, v1x, v2x, v0y, v1y, v2y, v0w, v1w,
-                                             v2w, unnormalized_matrix_inverse);
+            ComputeUnnormalizedMatrixInverse(vx0, vx1, vx2, vy0, vy1, vy2, vw0, vw1,
+                                             vw2, unnormalized_matrix_inverse);
 
-            // Initialize the bounding box to the entire screen.
+            // Project the vertices to pixel coordinates
             int left = 0, right = image_width, bottom = 0, top = image_height;
-            // If the triangle is entirely inside the screen, project the vertices to
-            // pixel coordinates and find the triangle bounding box enlarged to the
-            // nearest integer and clamped to the image boundaries.
-            if (v0w > 0 && v1w > 0 && v2w > 0) {
-                const float p0x = (v0x / v0w + 1.0) * half_image_width;
-                const float p1x = (v1x / v1w + 1.0) * half_image_width;
-                const float p2x = (v2x / v2w + 1.0) * half_image_width;
-                const float p0y = (v0y / v0w + 1.0) * half_image_height;
-                const float p1y = (v1y / v1w + 1.0) * half_image_height;
-                const float p2y = (v2y / v2w + 1.0) * half_image_height;
-                left = ClampedIntegerMin(p0x, p1x, p2x, 0, image_width);
-                right = ClampedIntegerMax(p0x, p1x, p2x, 0, image_width);
-                bottom = ClampedIntegerMin(p0y, p1y, p2y, 0, image_height);
-                top = ClampedIntegerMax(p0y, p1y, p2y, 0, image_height);
+            if (vw0 > 0 && vw1 > 0 && vw2 > 0) {
+                const float pixel_x0 = (vx0 / vw0 + 1.0) * half_image_width;
+                const float pixel_x1 = (vx1 / vw1 + 1.0) * half_image_width;
+                const float pixel_x2 = (vx2 / vw2 + 1.0) * half_image_width;
+                const float pixel_y0 = (vy0 / vw0 + 1.0) * half_image_height;
+                const float pixel_y1 = (vy1 / vw1 + 1.0) * half_image_height;
+                const float pixel_y2 = (vy2 / vw2 + 1.0) * half_image_height;
+                left = ClampedIntegerMin(pixel_x0, pixel_x1, pixel_x2, 0, image_width);
+                right = ClampedIntegerMax(pixel_x0, pixel_x1, pixel_x2, 0, image_width);
+                bottom = ClampedIntegerMin(pixel_y0, pixel_y1, pixel_y2, 0, image_height);
+                top = ClampedIntegerMax(pixel_y0, pixel_y1, pixel_y2, 0, image_height);
             }
 
             // Iterate over each pixel in the bounding box.
             for (int iy = bottom; iy < top; ++iy) {
                 for (int ix = left; ix < right; ++ix) {
-                    const float px = ((ix + 0.5) / half_image_width) - 1.0;
-                    const float py = ((iy + 0.5) / half_image_height) - 1.0;
+                    const float pixel_x = ((ix + 0.5) / half_image_width) - 1.0;
+                    const float pixel_y = ((iy + 0.5) / half_image_height) - 1.0;
                     const int pixel_idx = iy * image_width + ix;
 
-                    ComputeEdgeFunctions(px, py, unnormalized_matrix_inverse, b_over_w);
-                    if (!PixelIsInsideTriangle(b_over_w)) {
+                    ComputeEdgeFunctions(pixel_x, pixel_y, unnormalized_matrix_inverse, edge_w);
+                    if (!PixelIsInsideTriangle(edge_w)) {
                         continue;
                     }
 
-                    const float one_over_w = b_over_w[0] + b_over_w[1] + b_over_w[2];
-                    const float b0 = b_over_w[0] / one_over_w;
-                    const float b1 = b_over_w[1] / one_over_w;
-                    const float b2 = b_over_w[2] / one_over_w;
+                    const float edge_w_sum = edge_w[0] + edge_w[1] + edge_w[2];
+                    const float bc0 = edge_w[0] / edge_w_sum;
+                    const float bc1 = edge_w[1] / edge_w_sum;
+                    const float bc2 = edge_w[2] / edge_w_sum;
 
-                    const float v0z = vertices[v0_x_id + 2];
-                    const float v1z = vertices[v1_x_id + 2];
-                    const float v2z = vertices[v2_x_id + 2];
-                    // Since we computed an unnormalized w above, we need to recompute
-                    // a properly scaled clip-space w value and then divide clip-space z
-                    // by that.
-                    const float clip_z = b0 * v0z + b1 * v1z + b2 * v2z;
-                    const float clip_w = b0 * v0w + b1 * v1w + b2 * v2w;
+                    const float vz0 = vertices[vx0_id + 2];
+                    const float vz1 = vertices[vx1_id + 2];
+                    const float vz2 = vertices[vx2_id + 2];
+                    // Recompute a properly scaled clip-space w value and then divide clip-space z by that.
+                    const float clip_z = bc0 * vz0 + bc1 * vz1 + bc2 * vz2;
+                    const float clip_w = bc0 * vw0 + bc1 * vw1 + bc2 * vw2;
                     const float z = clip_z / clip_w;
 
-                    // Skip the pixel if it is farther than the current z-buffer pixel or
-                    // beyond the near or far clipping plane.
+                    // Skip the pixel which is farther than the current z-buffer pixel or beyond the near or far clipping plane.
                     if (z < -1.0 || z > 1.0 || z > z_buffer[pixel_idx]) {
                         continue;
                     }
 
                     triangle_ids[pixel_idx] = triangle_id;
                     z_buffer[pixel_idx] = z;
-                    barycentric_coordinates[3 * pixel_idx + 0] = b0;
-                    barycentric_coordinates[3 * pixel_idx + 1] = b1;
-                    barycentric_coordinates[3 * pixel_idx + 2] = b2;
+                    barycentric_coordinates[3 * pixel_idx + 0] = bc0;
+                    barycentric_coordinates[3 * pixel_idx + 1] = bc1;
+                    barycentric_coordinates[3 * pixel_idx + 2] = bc2;
                 }
             }
         }
@@ -262,6 +225,7 @@ public:
         const float* df_barycentric_coordinates_data = df_barycentric_coordinates_eigen.data();
         float* df_vertices_data = df_vertices_eigen.data();
 
+        //Start computing
         std::fill(df_vertices_data, df_vertices_data + vertex_count *4, 0.0f);
 
         for (unsigned int pixel_id = 0; pixel_id < image_height * image_width; ++pixel_id) {
--- test_rasterize_triangles.py
+++ test_rasterize_triangles.py
@@ -1,46 +1,45 @@
 
-# Copyright... 
+# Copixel_yright... 
 
 #import unittest
 #from op_test import OpTest
 
-import numpy as np
+import numpixel_y as np
 import math
 
-def ComputeUnnormalizedMatrixInverse(a11, a12, a13, a21, a22, a23, a31, a32, a33):
+def ComputeUnnormalizedMatrixInverse(m11, m12, m13, m21, m22, m23, m31, m32, m33):
     res = np.zeros((9))
-    res[0] = a22 * a33 - a32 * a23
-    res[1] = a13 * a32 - a33 * a12
-    res[2] = a12 * a23 - a22 * a13
-    res[3] = a23 * a31 - a33 * a21
-    res[4] = a11 * a33 - a31 * a13
-    res[5] = a13 * a21 - a23 * a11
-    res[6] = a21 * a32 - a31 * a22
-    res[7] = a12 * a31 - a32 * a11
-    res[8] = a11 * a22 - a21 * a12
+    res[0] = m22 * m33 - m32 * m23
+    res[1] = m13 * m32 - m33 * m12
+    res[2] = m12 * m23 - m22 * m13
+    res[3] = m23 * m31 - m33 * m21
+    res[4] = m11 * m33 - m31 * m13
+    res[5] = m13 * m21 - m23 * m11
+    res[6] = m21 * m32 - m31 * m22
+    res[7] = m12 * m31 - m32 * m11
+    res[8] = m11 * m22 - m21 * m12
 
-    det = a11 * res[0] + a12 * res[3] + a13 * res[6]
+    det = m11 * res[0] + m12 * res[3] + m13 * res[6]
 
     if det < 0.0:
         for i in range(9):
             res[i] = -res[i]
     return res
 
-def ComputeEdgeFunctions(px, py, m_inv):
+def ComputeEdgeFunctions(pixel_x, pixel_y, m_inv):
     res = np.zeros((3))
     for i in range(3):
         a = m_inv[3 * i + 0]
         b = m_inv[3 * i + 1]
         c = m_inv[3 * i + 2]
 
-        res[i] = a * px + b * py + c
+        res[i] = a * pixel_x + b * pixel_y + c
     return res
 
 def PixelIsInsideTriangle(value):
     return (value[0] >= 0 and value[1] >= 0 and value[2] >= 0) and (value[0] > 0 or value[1] > 0 or value[2] > 0)
 
 def ComputeRasterizeTriangles(vertices, triangles, image_height, image_width):
-    # init output
     barycentric_coordinates = np.zeros((image_height, image_width, 3), dtype=np.float32).flatten()
     triangle_ids = np.zeros((image_height, image_width), dtype=np.int32).flatten()
     z_buffer = np.ones((image_height, image_width), dtype=np.float32).flatten()
@@ -53,25 +52,25 @@ def ComputeRasterizeTriangles(vertices, triangles, image_height, image_width):
 
     for triangle_id in range(triangle_count):
         #compute for each triangle in mesh
-        v0_x_id = 4 * triangles[3 * triangle_id];
-        v1_x_id = 4 * triangles[3 * triangle_id + 1];
-        v2_x_id = 4 * triangles[3 * triangle_id + 2];
+        vx0_id = 4 * triangles[3 * triangle_id];
+        vx1_id = 4 * triangles[3 * triangle_id + 1];
+        vx2_id = 4 * triangles[3 * triangle_id + 2];
 
-        v0w = vertices[v0_x_id + 3]  #get the last one in each row of vertices, w from xyzw
-        v1w = vertices[v1_x_id + 3] 
-        v2w = vertices[v2_x_id + 3]
+        vw0 = vertices[vx0_id + 3]  #get the last one in each row of vertices, w from xyzw
+        vw1 = vertices[vx1_id + 3] 
+        vw2 = vertices[vx2_id + 3]
         
-        if (v0w < 0 and v1w < 0 and v2w < 0):
+        if (vw0 < 0 and vw1 < 0 and vw2 < 0):
             continue
 
-        v0x = vertices[v0_x_id];
-        v0y = vertices[v0_x_id + 1];
-        v1x = vertices[v1_x_id];
-        v1y = vertices[v1_x_id + 1];
-        v2x = vertices[v2_x_id];
-        v2y = vertices[v2_x_id + 1];
+        vx0 = vertices[vx0_id];
+        vy0 = vertices[vx0_id + 1];
+        vx1 = vertices[vx1_id];
+        vy1 = vertices[vx1_id + 1];
+        vx2 = vertices[vx2_id];
+        vy2 = vertices[vx2_id + 1];
 
-        unnormalized_matrix_inverse = ComputeUnnormalizedMatrixInverse(v0x, v1x, v2x, v0y, v1y, v2y, v0w, v1w, v2w);
+        unnormalized_matrix_inverse = ComputeUnnormalizedMatrixInverse(vx0, vx1, vx2, vy0, vy1, vy2, vw0, vw1, vw2);
 
         #Initialize the bounding box to the entire screen
         left = 0
@@ -79,54 +78,54 @@ def ComputeRasterizeTriangles(vertices, triangles, image_height, image_width):
         bottom = 0
         top = image_height
 
-        if (v0w > 0 and v1w > 0 and v2w > 0):
-            p0x = (v0x / v0w + 1.0) * half_image_width
-            p1x = (v1x / v1w + 1.0) * half_image_width
-            p2x = (v2x / v2w + 1.0) * half_image_width
-            p0y = (v0y / v0w + 1.0) * half_image_height
-            p1y = (v1y / v1w + 1.0) * half_image_height
-            p2y = (v2y / v2w + 1.0) * half_image_height
+        if (vw0 > 0 and vw1 > 0 and vw2 > 0):
+            pixel_x0 = (vx0 / vw0 + 1.0) * half_image_width
+            pixel_x1 = (vx1 / vw1 + 1.0) * half_image_width
+            pixel_x2 = (vx2 / vw2 + 1.0) * half_image_width
+            pixel_y0 = (vy0 / vw0 + 1.0) * half_image_height
+            pixel_y1 = (vy1 / vw1 + 1.0) * half_image_height
+            pixel_y2 = (vy2 / vw2 + 1.0) * half_image_height
 
-            left = min(max(int(math.floor(min(p0x, p1x, p2x))), 0), image_width)
-            right = min(max(int(math.ceil(max(p0x, p1x, p2x))), 0), image_width)
-            bottom = min(max(int(math.floor(min(p0y, p1y, p2y))), 0), image_height)
-            top = min(max(int(math.ceil(max(p0y, p1y, p2y))), 0), image_height)
+            left = min(max(int(math.floor(min(pixel_x0, pixel_x1, pixel_x2))), 0), image_width)
+            right = min(max(int(math.ceil(max(pixel_x0, pixel_x1, pixel_x2))), 0), image_width)
+            bottom = min(max(int(math.floor(min(pixel_y0, pixel_y1, pixel_y2))), 0), image_height)
+            top = min(max(int(math.ceil(max(pixel_y0, pixel_y1, pixel_y2))), 0), image_height)
 
         #Iterate over each pixel in the bounding box.
         for iy in range(bottom, top):
             for ix in range(left, right):
-                px = ((ix + 0.5) / half_image_width) - 1.0
-                py = ((iy + 0.5) / half_image_height) - 1.0
+                pixel_x = ((ix + 0.5) / half_image_width) - 1.0
+                pixel_y = ((iy + 0.5) / half_image_height) - 1.0
                 pixel_idx = int(iy * half_image_width + ix)
 
-                b_over_w = ComputeEdgeFunctions(px, py, unnormalized_matrix_inverse)
+                edge_w = ComputeEdgeFunctions(pixel_x, pixel_y, unnormalized_matrix_inverse)
 
-                if not PixelIsInsideTriangle(b_over_w):
+                if not PixelIsInsideTriangle(edge_w):
                     continue
 
-                bs_over_w = b_over_w[0] + b_over_w[1] + b_over_w[2]
-                b0 = b_over_w[0] / bs_over_w
-                b1 = b_over_w[1] / bs_over_w
-                b2 = b_over_w[2] / bs_over_w
+                edge_w_sum = edge_w[0] + edge_w[1] + edge_w[2]
+                bc0 = edge_w[0] / edge_w_sum
+                bc1 = edge_w[1] / edge_w_sum
+                bc2 = edge_w[2] / edge_w_sum
 
-                v0z = vertices[v0_x_id + 2]
-                v1z = vertices[v1_x_id + 2]
-                v2z = vertices[v2_x_id + 2]
+                vz0 = vertices[vx0_id + 2]
+                vz1 = vertices[vx1_id + 2]
+                vz2 = vertices[vx2_id + 2]
 
-                clip_z = b0 * v0z + b1 * v1z + b2 * v2z
-                clip_w = b0 * v0w + b1 * v1w + b2 * v2w
+                clip_z = bc0 * vz0 + bc1 * vz1 + bc2 * vz2
+                clip_w = bc0 * vw0 + bc1 * vw1 + bc2 * vw2
                 z = clip_z / clip_w
                 if z < -1.0 or z > 1.0 or z > z_buffer[pixel_idx]:
                     continue
 
                 triangle_ids[pixel_idx] = triangle_id
                 z_buffer[pixel_idx] = z
-                barycentric_coordinates[3 * pixel_idx + 0] = b0
-                barycentric_coordinates[3 * pixel_idx + 1] = b1
-                barycentric_coordinates[3 * pixel_idx + 2] = b2
-    print('barycentric_coordinates', barycentric_coordinates)
-    print('triangle_ids', triangle_ids)
-    print('z_buffer', z_buffer)
+                barycentric_coordinates[3 * pixel_idx + 0] = bc0
+                barycentric_coordinates[3 * pixel_idx + 1] = bc1
+                barycentric_coordinates[3 * pixel_idx + 2] = bc2
+#    print('barycentric_coordinates', barycentric_coordinates)
+#    print('triangle_ids', triangle_ids)
+#    print('z_buffer', z_buffer)
     return barycentric_coordinates, triangle_ids, z_buffer
 
 class TestRasterizeTrianglesOp():  #OpTest
@@ -152,6 +151,6 @@ class TestRasterizeTrianglesOp():  #OpTest
     def test_check_grad_normal(self):
         self.check_grad(['Vertices'], 'BarycentricCoordinates', max_relative_error=0.005)
 
-if __name__ == '__main__':
-    c = TestRasterizeTrianglesOp()
-    c.setUp()
+#if __name__ == '__main__':
+#    c = TestRasterizeTrianglesOp()
+#    c.setUp()
